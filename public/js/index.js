9319353370e0444d035d4f1ab00384377c3a402e
Jakob <accounts@jakoblangenbahn.com>
2021-03-28 11:47:40 +0200

    Add time and new logic

--- public/js/index.js
+++ public/js/index.js
@@ -10,13 +10,15 @@ function onResolve(foundURL) {
 
 // onReject function
 function onReject(error) {
-    random_logo(min, max)
+    random_logo(min_logo, max_logo,min_animation,max_animation)
 }
 
 // function for loading next image
-function random_logo(min, max) {
-    number = Math.floor(Math.random() * (max - min + 1) + min);
-    animation_file = 'animation/Animation_' + number + '.svg';
+function random_logo(min_logo, max_logo,min_animation,max_animation) {
+
+    number_logo = Math.floor(Math.random() * (max_logo - min_logo + 1) + min_logo);
+    number_animation = Math.floor(Math.random() * (max_animation - min_animation + 1) + min_animation);
+    animation_file = 'animation/logo_' + number_logo + '_animation_' + number_animation + '.svg'
     storageRef.child(animation_file).getDownloadURL().then(onResolve, onReject);
 
 }
@@ -34,13 +36,13 @@ function update_highscore_load_new_image(alias) {
     storyRef.set({score: increment, alias: alias}, {merge: true});
 
     // get new image
-    random_logo(min, max)
+    random_logo(min_logo, max_logo,min_animation,max_animation)
 
 }
 
 // add label functions
 function no_logo() {
-    random_logo(min, max)
+    random_logo(min_logo, max_logo,min_animation,max_animation)
 }
 
 // add label functions
@@ -49,7 +51,8 @@ function add_label_very_bad(animation_file) {
     db.collection("label").doc().set({
         logo: animation_file,
         rating: "Very Bad",
-        alias: alias
+        alias: alias,
+        time: firebase.firestore.FieldValue.serverTimestamp()
     })
     update_highscore_load_new_image(alias)
 }
@@ -59,7 +62,8 @@ function add_label_bad(animation_file) {
     db.collection("label").doc().set({
         logo: animation_file,
         rating: "Bad",
-        alias: alias
+        alias: alias,
+        time: firebase.firestore.FieldValue.serverTimestamp()
     })
     update_highscore_load_new_image(alias)
 }
@@ -69,7 +73,8 @@ function add_label_okay(animation_file) {
     db.collection("label").doc().set({
         logo: animation_file,
         rating: "Okay",
-        alias: alias
+        alias: alias,
+        time: firebase.firestore.FieldValue.serverTimestamp()
     })
     update_highscore_load_new_image(alias)
 }
@@ -79,7 +84,8 @@ function add_label_good(animation_file) {
     db.collection("label").doc().set({
         logo: animation_file,
         rating: "Good",
-        alias: alias
+        alias: alias,
+        time: firebase.firestore.FieldValue.serverTimestamp()
     })
     update_highscore_load_new_image(alias)
 }
@@ -89,16 +95,20 @@ function add_label_very_good(animation_file) {
     db.collection("label").doc().set({
         logo: animation_file,
         rating: "Very Good",
-        alias: alias
+        alias: alias,
+        time: firebase.firestore.FieldValue.serverTimestamp()
     })
     update_highscore_load_new_image(alias)
 }
 
 // Initiate random image
-let min = 0;
-let max = 8;
-let number = Math.floor(Math.random() * (max - min + 1) + min);
-let animation_file = 'animation/Animation_' + number + '.svg'
+let min_logo = 0;
+let max_logo = 191;
+let number_logo = Math.floor(Math.random() * (max_logo - min_logo + 1) + min_logo);
+let min_animation = 0;
+let max_animation = 49;
+let number_animation = Math.floor(Math.random() * (max_animation - min_animation + 1) + min_animation);
+let animation_file = 'animation/logo_' + number_logo + '_animation_' + number_animation + '.svg'
 
 // show initial animation
 storageRef.child(animation_file).getDownloadURL().then(onResolve, onReject);
