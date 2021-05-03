6bc64b4590aaacafd428f537876e3b971f553943
Jakob <accounts@jakoblangenbahn.com>
2021-05-03 15:21:14 +0200

    Clean up code

--- public/js/highscores.js
+++ public/js/highscores.js
@@ -1,18 +1,21 @@
-var db = firebase.firestore();
-db.collection('highscore').orderBy("score", "desc").limit(20).get().then((snapshot) => {
+/* Import the names and scores of the 20 people who have labelled the most */
+let db = firebase.firestore();
+db.collection("highscore").orderBy("score", "desc").limit(20).get().then((snapshot) => {
     snapshot.docs.forEach(doc => {
         renderAccount(doc);
     })
 })
 
-const accountList = document.querySelector('#table_highscore');
+
+/* Display the imported data in a high score table */
+const accountList = document.querySelector("#table_highscore");
 
 function renderAccount(doc) {
-    let tr = document.createElement('tr');
-    let td_alias = document.createElement('td');
-    let td_score = document.createElement('td');
+    let tr = document.createElement("tr");
+    let td_alias = document.createElement("td");
+    let td_score = document.createElement("td");
 
-    tr.setAttribute('data-id', doc.id);
+    tr.setAttribute("data-id", doc.id);
     td_alias.textContent = doc.data().alias;
     td_score.textContent = doc.data().score;
 
@@ -20,38 +23,4 @@ function renderAccount(doc) {
     tr.appendChild(td_score);
 
     accountList.appendChild(tr);
-
 }
-
-// Quick and simple export target #table_id into a csv
-function download_table_as_csv(table_id, separator = ',') {
-    // Select rows from table_id
-    var rows = document.querySelectorAll('table#' + table_id + ' tr');
-    // Construct csv
-    var csv = [];
-    for (var i = 0; i < rows.length; i++) {
-        var row = [], cols = rows[i].querySelectorAll('td, th');
-        for (var j = 0; j < cols.length; j++) {
-            // Clean innertext to remove multiple spaces and jumpline (break csv)
-            var data = cols[j].innerText.replace(/(\r\n|\n|\r)/gm, '').replace(/(\s\s)/gm, ' ')
-            // Escape double-quote with double-double-quote (see https://stackoverflow.com/questions/17808511/properly-escape-a-double-quote-in-csv)
-            data = data.replace(/"/g, '""');
-            // Push escaped string
-            row.push('"' + data + '"');
-        }
-        csv.push(row.join(separator));
-    }
-    var csv_string = csv.join('\n');
-    // Download it
-    var filename = 'export_' + table_id + '_' + new Date().toLocaleDateString() + '.csv';
-    var link = document.createElement('a');
-    link.style.display = 'none';
-    link.setAttribute('target', '_blank');
-    link.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(csv_string));
-    link.setAttribute('download', filename);
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-}
-
-
--- public/js/label-paths.js
+++ public/js/label-paths.js
@@ -1,8 +1,250 @@
-// Connect to database
+/* Define firestore reference for easy use in functions */
 const db = firebase.firestore();
 const storage = firebase.storage();
 
-// Initiate first image
+
+/* Update highscore and load the paths images of the next Logo  */
+function update_highscore_load_new_image(alias) {
+    const increment = firebase.firestore.FieldValue.increment(1);
+    const storyRef = db.collection('highscore').doc(alias);
+    storyRef.set({score: increment, alias: alias}, {merge: true});
+
+    document.getElementById("path_0").style.border = "none";
+    document.getElementById("path_1").style.border = "none";
+    document.getElementById("path_2").style.border = "none";
+    document.getElementById("path_3").style.border = "none";
+    document.getElementById("path_4").style.border = "none";
+    document.getElementById("path_5").style.border = "none";
+    document.getElementById("path_6").style.border = "none";
+    document.getElementById("path_7").style.border = "none";
+
+    document.getElementById("path_0").onclick = add_label_path_0;
+    document.getElementById("path_1").onclick = add_label_path_1;
+    document.getElementById("path_2").onclick = add_label_path_2;
+    document.getElementById("path_3").onclick = add_label_path_3;
+    document.getElementById("path_4").onclick = add_label_path_4;
+    document.getElementById("path_5").onclick = add_label_path_5;
+    document.getElementById("path_6").onclick = add_label_path_6;
+    document.getElementById("path_7").onclick = add_label_path_7;
+
+    /* Increase the number so that every Logo is displayed in order based on their number */
+    number++;
+    logo_file_path_full = 'path/logo_' + number + "_path_full.svg";
+    logo_file_path_0 = 'path/logo_' + number + "_path_0.svg";
+    logo_file_path_1 = 'path/logo_' + number + "_path_1.svg";
+    logo_file_path_2 = 'path/logo_' + number + "_path_2.svg";
+    logo_file_path_3 = 'path/logo_' + number + "_path_3.svg";
+    logo_file_path_4 = 'path/logo_' + number + "_path_4.svg";
+    logo_file_path_5 = 'path/logo_' + number + "_path_5.svg";
+    logo_file_path_6 = 'path/logo_' + number + "_path_6.svg";
+    logo_file_path_7 = 'path/logo_' + number + "_path_7.svg";
+
+    storage.ref(logo_file_path_full).getDownloadURL()
+        .then((url) => {
+            document.getElementById("logo_full").src = url;
+        })
+
+    storage.ref(logo_file_path_0).getDownloadURL()
+        .then((url_path_0) => {
+            document.getElementById("image_path_0").src = url_path_0;
+        })
+
+    storage.ref(logo_file_path_1).getDownloadURL()
+        .then((url_path_1) => {
+            document.getElementById("image_path_1").src = url_path_1;
+        })
+
+    storage.ref(logo_file_path_2).getDownloadURL()
+        .then((url_path_2) => {
+            document.getElementById("image_path_2").src = url_path_2;
+        })
+
+    storage.ref(logo_file_path_3).getDownloadURL()
+        .then((url_path_3) => {
+            document.getElementById("image_path_3").src = url_path_3;
+        })
+
+    storage.ref(logo_file_path_4).getDownloadURL()
+        .then((url_path_4) => {
+            document.getElementById("image_path_4").src = url_path_4;
+        })
+
+    storage.ref(logo_file_path_5).getDownloadURL()
+        .then((url_path_5) => {
+            document.getElementById("image_path_5").src = url_path_5;
+        })
+
+    storage.ref(logo_file_path_6).getDownloadURL()
+        .then((url_path_6) => {
+            document.getElementById("image_path_6").src = url_path_6;
+        })
+
+    storage.ref(logo_file_path_7).getDownloadURL()
+        .then((url_path_7) => {
+            document.getElementById("image_path_7").src = url_path_7;
+        })
+
+    animate_logo_0 = false;
+    animate_logo_1 = false;
+    animate_logo_2 = false;
+    animate_logo_3 = false;
+    animate_logo_4 = false;
+    animate_logo_5 = false;
+    animate_logo_6 = false;
+    animate_logo_7 = false;
+
+}
+
+
+/* Add the label "true" in the database for path 0 */
+function add_label_path_0() {
+    animate_logo_0 = true;
+    document.getElementById("path_0").style.border = "1px solid black";
+    document.getElementById("path_0").onclick = delete_label_path_0;
+}
+
+
+/* Add the label "false" in the database for path 0 */
+function delete_label_path_0() {
+    animate_logo_0 = false;
+    document.getElementById("path_0").style.border = "none";
+    document.getElementById("path_0").onclick = add_label_path_0;
+}
+
+
+/* Add the label "true" in the database for path 1 */
+function add_label_path_1() {
+    animate_logo_1 = true;
+    document.getElementById("path_1").style.border = "1px solid black";
+    document.getElementById("path_1").onclick = delete_label_path_1;
+}
+
+
+/* Add the label "false" in the database for path 1 */
+function delete_label_path_1() {
+    animate_logo_1 = false;
+    document.getElementById("path_1").style.border = "none";
+    document.getElementById("path_1").onclick = add_label_path_1;
+}
+
+
+/* Add the label "true" in the database for path 2 */
+function add_label_path_2() {
+    animate_logo_2 = true;
+    document.getElementById("path_2").style.border = "1px solid black";
+    document.getElementById("path_2").onclick = delete_label_path_2;
+}
+
+
+/* Add the label "false" in the database for path 2 */
+function delete_label_path_2() {
+    animate_logo_2 = false;
+    document.getElementById("path_2").style.border = "none";
+    document.getElementById("path_2").onclick = add_label_path_2;
+}
+
+
+/* Add the label "true" in the database for path 3 */
+function add_label_path_3() {
+    animate_logo_3 = true;
+    document.getElementById("path_3").style.border = "1px solid black";
+    document.getElementById("path_3").onclick = delete_label_path_3;
+}
+
+
+/* Add the label "false" in the database for path 3 */
+function delete_label_path_3() {
+    animate_logo_3 = false;
+    document.getElementById("path_3").style.border = "none";
+    document.getElementById("path_3").onclick = add_label_path_3;
+}
+
+
+/* Add the label "true" in the database for path 4 */
+function add_label_path_4() {
+    animate_logo_4 = true;
+    document.getElementById("path_4").style.border = "1px solid black";
+    document.getElementById("path_4").onclick = delete_label_path_4;
+}
+
+
+/* Add the label "false" in the database for path 4 */
+function delete_label_path_4() {
+    animate_logo_4 = false;
+    document.getElementById("path_4").style.border = "none";
+    document.getElementById("path_4").onclick = add_label_path_4;
+}
+
+
+/* Add the label "true" in the database for path 5 */
+function add_label_path_5() {
+    animate_logo_5 = true;
+    document.getElementById("path_5").style.border = "1px solid black";
+    document.getElementById("path_5").onclick = delete_label_path_5;
+}
+
+
+/* Add the label "false" in the database for path 5 */
+function delete_label_path_5() {
+    animate_logo_5 = false;
+    document.getElementById("path_5").style.border = "none";
+    document.getElementById("path_5").onclick = add_label_path_5;
+}
+
+
+/* Add the label "true" in the database for path 6 */
+function add_label_path_6() {
+    animate_logo_6 = true;
+    document.getElementById("path_6").style.border = "1px solid black";
+    document.getElementById("path_6").onclick = delete_label_path_6;
+}
+
+
+/* Add the label "false" in the database for path 6 */
+function delete_label_path_6() {
+    animate_logo_6 = false;
+    document.getElementById("path_6").style.border = "none";
+    document.getElementById("path_6").onclick = add_label_path_6;
+}
+
+
+/* Add the label "true" in the database for path 7 */
+function add_label_path_7() {
+    animate_logo_7 = true;
+    document.getElementById("path_7").style.border = "1px solid black";
+    document.getElementById("path_7").onclick = delete_label_path_7;
+}
+
+
+/* Add the label "false" in the database for path 7 */
+function delete_label_path_7() {
+    animate_logo_7 = false;
+    document.getElementById("path_7").style.border = "none";
+    document.getElementById("path_7").onclick = add_label_path_7;
+}
+
+
+/* Send data to database and execute function update_highscore_load_new_image */
+function send_data() {
+    let alias = document.getElementById("alias").value;
+    db.collection("labelpath").doc().set({
+        alias: alias,
+        logo: logo_file_path_full,
+        path_0: animate_logo_0,
+        path_1: animate_logo_1,
+        path_2: animate_logo_2,
+        path_3: animate_logo_3,
+        path_4: animate_logo_4,
+        path_5: animate_logo_5,
+        path_6: animate_logo_6,
+        path_7: animate_logo_7,
+        time: firebase.firestore.FieldValue.serverTimestamp()
+    })
+    update_highscore_load_new_image(alias)
+}
+
+
+/* Initialize the first 8 paths of the Logo with number 192 */
 let number = 192;
 let logo_file_path_full = 'path/logo_' + number + "_path_full.svg";
 let logo_file_path_0 = 'path/logo_' + number + "_path_0.svg";
@@ -13,7 +255,6 @@ let logo_file_path_4 = 'path/logo_' + number + "_path_4.svg";
 let logo_file_path_5 = 'path/logo_' + number + "_path_5.svg";
 let logo_file_path_6 = 'path/logo_' + number + "_path_6.svg";
 let logo_file_path_7 = 'path/logo_' + number + "_path_7.svg";
-
 
 storage.ref(logo_file_path_full).getDownloadURL()
     .then((url) => {
@@ -60,9 +301,6 @@ storage.ref(logo_file_path_7).getDownloadURL()
         document.getElementById("image_path_7").src = url_path_7;
     });
 
-
-
-// Initiate variables
 let animate_logo_0 = false;
 let animate_logo_1 = false;
 let animate_logo_2 = false;
@@ -71,218 +309,5 @@ let animate_logo_4 = false;
 let animate_logo_5 = false;
 let animate_logo_6 = false;
 let animate_logo_7 = false;
-
-// Change values
-function add_label_path_0() {
-    animate_logo_0 = true;
-    document.getElementById("path_0").style.border = "1px solid black";
-    document.getElementById("path_0").onclick = delete_label_path_0;
-}
-
-function delete_label_path_0() {
-    animate_logo_0 = false;
-    document.getElementById("path_0").style.border = "none";
-    document.getElementById("path_0").onclick = add_label_path_0;
-}
-
-function add_label_path_1() {
-    animate_logo_1 = true;
-    document.getElementById("path_1").style.border = "1px solid black";
-    document.getElementById("path_1").onclick = delete_label_path_1;
-}
-
-function delete_label_path_1() {
-    animate_logo_1 = false;
-    document.getElementById("path_1").style.border = "none";
-    document.getElementById("path_1").onclick = add_label_path_1;
-}
-
-function add_label_path_2() {
-    animate_logo_2 = true;
-    document.getElementById("path_2").style.border = "1px solid black";
-    document.getElementById("path_2").onclick = delete_label_path_2;
-}
-
-function delete_label_path_2() {
-    animate_logo_2 = false;
-    document.getElementById("path_2").style.border = "none";
-    document.getElementById("path_2").onclick = add_label_path_2;
-}
-
-function add_label_path_3() {
-    animate_logo_3 = true;
-    document.getElementById("path_3").style.border = "1px solid black";
-    document.getElementById("path_3").onclick = delete_label_path_3;
-}
-
-function delete_label_path_3() {
-    animate_logo_3 = false;
-    document.getElementById("path_3").style.border = "none";
-    document.getElementById("path_3").onclick = add_label_path_3;
-}
-
-function add_label_path_4() {
-    animate_logo_4 = true;
-    document.getElementById("path_4").style.border = "1px solid black";
-    document.getElementById("path_4").onclick = delete_label_path_4;
-}
-
-function delete_label_path_4() {
-    animate_logo_4 = false;
-    document.getElementById("path_4").style.border = "none";
-    document.getElementById("path_4").onclick = add_label_path_4;
-}
-
-function add_label_path_5() {
-    animate_logo_5 = true;
-    document.getElementById("path_5").style.border = "1px solid black";
-    document.getElementById("path_5").onclick = delete_label_path_5;
-}
-
-function delete_label_path_5() {
-    animate_logo_5 = false;
-    document.getElementById("path_5").style.border = "none";
-    document.getElementById("path_5").onclick = add_label_path_5;
-}
-
-function add_label_path_6() {
-    animate_logo_6 = true;
-    document.getElementById("path_6").style.border = "1px solid black";
-    document.getElementById("path_6").onclick = delete_label_path_6;
-}
-
-function delete_label_path_6() {
-    animate_logo_6 = false;
-    document.getElementById("path_6").style.border = "none";
-    document.getElementById("path_6").onclick = add_label_path_6;
-}
-
-function add_label_path_7() {
-    animate_logo_7 = true;
-    document.getElementById("path_7").style.border = "1px solid black";
-    document.getElementById("path_7").onclick = delete_label_path_7;
-}
-
-function delete_label_path_7() {
-    animate_logo_7 = false;
-    document.getElementById("path_7").style.border = "none";
-    document.getElementById("path_7").onclick = add_label_path_7;
-}
-
-
-
-
-// send button
-function send_data() {
-    let alias = document.getElementById("alias").value;
-    db.collection("labelpath").doc().set({
-        alias: alias,
-        logo: logo_file_path_full,
-        path_0: animate_logo_0,
-        path_1: animate_logo_1,
-        path_2: animate_logo_2,
-        path_3: animate_logo_3,
-        path_4: animate_logo_4,
-        path_5: animate_logo_5,
-        path_6: animate_logo_6,
-        path_7: animate_logo_7,
-        time: firebase.firestore.FieldValue.serverTimestamp()
-    })
-    update_highscore_load_new_image(alias)
-}
-
-
-// update highscore
-function update_highscore_load_new_image(alias) {
-    const increment = firebase.firestore.FieldValue.increment(1);
-    const storyRef = db.collection('highscore').doc(alias);
-    storyRef.set({score: increment, alias: alias}, {merge: true});
-
-    // delete marker
-    document.getElementById("path_0").style.border =  "none";
-    document.getElementById("path_1").style.border =  "none";
-    document.getElementById("path_2").style.border =  "none";
-    document.getElementById("path_3").style.border =  "none";
-    document.getElementById("path_4").style.border =  "none";
-    document.getElementById("path_5").style.border =  "none";
-    document.getElementById("path_6").style.border =  "none";
-    document.getElementById("path_7").style.border =  "none";
-
-    // add function
-    document.getElementById("path_0").onclick = add_label_path_0;
-    document.getElementById("path_1").onclick = add_label_path_1;
-    document.getElementById("path_2").onclick = add_label_path_2;
-    document.getElementById("path_3").onclick = add_label_path_3;
-    document.getElementById("path_4").onclick = add_label_path_4;
-    document.getElementById("path_5").onclick = add_label_path_5;
-    document.getElementById("path_6").onclick = add_label_path_6;
-    document.getElementById("path_7").onclick = add_label_path_7;
-
-
-
-    // get new image
-    number++;
-    logo_file_path_full = 'path/logo_' + number + "_path_full.svg";
-    logo_file_path_0 = 'path/logo_' + number + "_path_0.svg";
-    logo_file_path_1 = 'path/logo_' + number + "_path_1.svg";
-    logo_file_path_2 = 'path/logo_' + number + "_path_2.svg";
-    logo_file_path_3 = 'path/logo_' + number + "_path_3.svg";
-    logo_file_path_4 = 'path/logo_' + number + "_path_4.svg";
-    logo_file_path_5 = 'path/logo_' + number + "_path_5.svg";
-    logo_file_path_6 = 'path/logo_' + number + "_path_6.svg";
-    logo_file_path_7 = 'path/logo_' + number + "_path_7.svg";
-
-
-
-    storage.ref(logo_file_path_full).getDownloadURL()
-        .then((url) => {
-            document.getElementById("logo_full").src = url;
-        })
-    storage.ref(logo_file_path_0).getDownloadURL()
-        .then((url_path_0) => {
-            document.getElementById("image_path_0").src = url_path_0;
-        })
-    storage.ref(logo_file_path_1).getDownloadURL()
-        .then((url_path_1) => {
-            document.getElementById("image_path_1").src = url_path_1;
-        })
-    storage.ref(logo_file_path_2).getDownloadURL()
-        .then((url_path_2) => {
-            document.getElementById("image_path_2").src = url_path_2;
-        })
-    storage.ref(logo_file_path_3).getDownloadURL()
-        .then((url_path_3) => {
-            document.getElementById("image_path_3").src = url_path_3;
-        })
-    storage.ref(logo_file_path_4).getDownloadURL()
-        .then((url_path_4) => {
-            document.getElementById("image_path_4").src = url_path_4;
-        })
-    storage.ref(logo_file_path_5).getDownloadURL()
-        .then((url_path_5) => {
-            document.getElementById("image_path_5").src = url_path_5;
-        })
-    storage.ref(logo_file_path_6).getDownloadURL()
-        .then((url_path_6) => {
-            document.getElementById("image_path_6").src = url_path_6;
-        })
-    storage.ref(logo_file_path_7).getDownloadURL()
-        .then((url_path_7) => {
-            document.getElementById("image_path_7").src = url_path_7;
-        })
-
-
-// Reset variables
-    animate_logo_0 = false;
-    animate_logo_1 = false;
-    animate_logo_2 = false;
-    animate_logo_3 = false;
-    animate_logo_4 = false;
-    animate_logo_5 = false;
-    animate_logo_6 = false;
-    animate_logo_7 = false;
-
-}
-
 
 
